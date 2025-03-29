f5f7611b657e1f869a4edd37c5409d939e99ae70
Simon890 <revellosimon890@gmail.com>
2025-03-29 14:34:02 -0300

    Add stripes

--- src/Memory.ts
+++ src/Memory.ts
@@ -1,4 +1,4 @@
-import { MEMORY_SIZE } from "./constants/memoryConstants";
+import { MEMORY_SIZE, STRIPE_CHARSET } from "./constants/memoryConstants";
 import { MemoryOutOfBounds } from "./errors/MemoryOutOfBounds";
 
 export class Memory {
@@ -12,6 +12,7 @@ export class Memory {
 
     public reset() {
         this._buffer.fill(0);
+        this._buffer.set(STRIPE_CHARSET, 0);
     }
 
     public setMemory(index : number, value : number) {
@@ -22,6 +23,12 @@ export class Memory {
     public getMemory(index : number) {
         this._validateMemory(index);
         return this._buffer[index];
+    }
+
+    public getOpcode(index : number) {
+        const highByte = this.getMemory(index);
+        const lowByte = this.getMemory(index + 1);
+        return (highByte << 8) | lowByte;
     }
 
     private _validateMemory(index : number) {
